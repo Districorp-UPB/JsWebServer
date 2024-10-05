0a14b6ba0699aa40da03d4432720efc8891cdd32
Sebastarazq <juansebastianhijomenor@hotmail.com>
2024-10-05 17:42:07 -0500

    cambios package

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const serverConfig = require("./config/serverConfig");
-const clientRoutes = require("./routes/clientRoutes");
+import express from "express";
+import serverConfig from "./config/serverConfig.js";
+import clientRoutes from "./routes/clientRoutes.js";
 
 const app = express();
 
@@ -13,7 +13,3 @@ app.use("/api", clientRoutes);
 app.listen(serverConfig.PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${serverConfig.PORT}`);
 });
-
-
-
-
