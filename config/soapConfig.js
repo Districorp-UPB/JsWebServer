08f60e007abd0788e81c9bbb01c7ba868e03436b
Valentina2103 <80237175+Valentina2103@users.noreply.github.com>
2024-10-08 17:05:50 -0500

    Update URL for communication with soap server

--- config/soapConfig.js
+++ config/soapConfig.js
@@ -1,7 +1,7 @@
 
 const soapConfig = {
     login: 'https://207.248.81.69:443/auth-api.php?wsdl',
-    register: 'https://207.248.81.69:443/regis-api.php?wsdl'
+    register: 'https://207.248.81.69:443/regis-api.php?wsdl' 
 };
 
 export default soapConfig;
